e91b1a0bf23b48e07ab5234b9a3dc9a3fd5871be
jose <jalonsoriveiro@danielcastelao.org>
2020-04-17 23:33:31 +0200

    Initial commit

---
+++ Tarea4.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author jose
+ */
+public class Tarea4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        
+        // create JFrame and JTable
+        JFrame frame = new JFrame();
+        JTable table = new JTable(); 
+        
+        // create a table model and set a Column Identifiers to this model 
+        Object[] columns = {"Nombre","Apellido","Curso"};
+        DefaultTableModel model = new DefaultTableModel();
+        model.setColumnIdentifiers(columns);
+        
+        // set the model to the table
+        table.setModel(model);
+        
+        // Change A JTable Background Color, Font Size, Font Color, Row Height
+        table.setBackground(Color.LIGHT_GRAY);
+        table.setForeground(Color.black);
+        Font font = new Font("",1,22);
+        table.setFont(font);
+        table.setRowHeight(30);
+        
+        // create JTextFields
+        JTextField textId = new JTextField();
+        JTextField textFname = new JTextField();
+        JTextField textLname = new JTextField();
+        JTextField textAge = new JTextField();
+        
+        // create JButtons
+        JButton btnAdd = new JButton("Add");
+        JButton btnDelete = new JButton("Delete");
+        JButton btnUpdate = new JButton("Update");     
+        
+        textId.setBounds(20, 20, 100, 25);
+        textFname.setBounds(20, 50, 100, 25);
+        textLname.setBounds(20, 80, 100, 25);
+        textAge.setBounds(20,110, 100, 25);
+        
+        btnAdd.setBounds(150, 20, 100, 25);
+        btnUpdate.setBounds(150, 50, 100, 25);
+        btnDelete.setBounds(150, 80, 100, 25);
+        
+        // create JScrollPane
+        JScrollPane pane = new JScrollPane(table);
+        pane.setBounds(20, 140, 780, 200);
+        
+        frame.setLayout(null);
+        
+        frame.add(pane);
+        
+        // add JTextFields to the jframe
+        frame.add(textId);
+        frame.add(textFname);
+        frame.add(textLname);
+        frame.add(textAge);
+    
+        // add JButtons to the jframe
+        frame.add(btnAdd);
+        frame.add(btnDelete);
+        frame.add(btnUpdate);
+        
+        // create an array of objects to set the row data
+        Object[] row = new Object[4];
+        
+        // button add row
+        btnAdd.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+             
+                row[0] = textId.getText();
+                row[1] = textFname.getText();
+                row[2] = textLname.getText();
+               // row[3] = textAge.getText();
+                
+                // add row to the model
+                model.addRow(row);
+            }
+            
+        });
+        
+        // button remove row
+        btnDelete.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            
+                // i = the index of the selected row
+                int i = table.getSelectedRow();
+                if(i >= 0){
+                    // remove a row from jtable
+                    model.removeRow(i);
+                }
+                else{
+                    System.out.println("Delete Error");
+                }
+            }
+        });
+        
+        // get selected row data From table to textfields 
+        table.addMouseListener(new MouseAdapter(){
+        
+        @Override
+        public void mouseClicked(MouseEvent e){
+            
+            // i = the index of the selected row
+            int i = table.getSelectedRow();
+            
+            textId.setText(model.getValueAt(i, 0).toString());
+            textFname.setText(model.getValueAt(i, 1).toString());
+            textLname.setText(model.getValueAt(i, 2).toString());
+           // textAge.setText(model.getValueAt(i, 3).toString());
+        }
+        });
+        
+        // button update row
+        btnUpdate.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+             
+                // i = the index of the selected row
+                int i = table.getSelectedRow();
+                
+                if(i >= 0) 
+                {
+                   model.setValueAt(textId.getText(), i, 0);
+                   model.setValueAt(textFname.getText(), i, 1);
+                   model.setValueAt(textLname.getText(), i, 2);
+                   //model.setValueAt(textAge.getText(), i, 3);
+                }
+                else{
+                    System.out.println("Update Error");
+                }
+            }
+        });
+        
+        frame.setSize(900,400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+    }
+}
+    
+
